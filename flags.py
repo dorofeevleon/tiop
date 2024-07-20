d73e738483aad6ad16f599fff0be7dccf234f954
dorofeevleon <159351827+dorofeevleon@users.noreply.github.com>
2024-07-20 17:37:51 +0300

    file

---
+++ flags.py
@@ -0,0 +1,11 @@
+"""Utility for flags"""
+from absl import flags
+
+
+def define_flags():
+  """Define flags for nin"""
+  flags.DEFINE_integer("epoch", 200, "The number of epochs. [50]")
+  flags.DEFINE_string("dataset", "cifar-10",
+                      "The name of dataset. [cifar-10, mnist]")
+  flags.DEFINE_integer("batch_size", 128, "The size of batch. [128]")
+  flags.DEFINE_float('learning_rate', 0.001, "Learning rate. [0.001]")
